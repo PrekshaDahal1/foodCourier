b55b609bb866c00b118c68a0f436f9d3d370bab7
Preksha Dahal <prekshadahal88@gmail.com>
2023-12-07 21:45:00 +0545

    Primary changes

--- FoodCourier/ViewController.swift
+++ FoodCourier/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Constants and Variables
+    
+    
+    // MARK: - Initialization
+    
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        setupLayout()
+        
     }
-
-
+    
+    // MARK: - View Will Appear
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    // MARK: - View Will Disappear
+    override func viewWillDisappear(_ animated: Bool) {
+        
+    }
+    
+    // MARK: - Additional Functions
+    
 }
 
+// MARK: - Autolayout for UICOmponents
+    extension ViewController {
+    
+    // MARK: - Setting Up Autolayout for UI Components
+    func setupLayout() {
+        
+        autoLayoutForLabel()
+        
+    }
+    
+    // MARK: - Autolayout
+    func autoLayoutForLabel(){
+        
+    }
+}
+// MARK: - Actions for UIComponents
+    extension ViewController {
+    
+    // MARK: - Setup Actions for UIComponents
+    func setupUIAction() {
+        
+    }
+    
+}
+
+    extension ViewController{
+    // MARK: - API Call
+    func apiCall() {
+        
+    }
+    
+}
